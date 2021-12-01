a265cf93026ec3c7b1bcc6f4031654b859dd8f31
Tony Ben <tony.ben@calix.com>
2021-12-01 17:46:58 +0800

    Mock User Login & Logout

--- backend/src/api/authz/user_mgmt.py
+++ backend/src/api/authz/user_mgmt.py
@@ -19,9 +19,11 @@ import logging
 
 logger=logging.getLogger(__name__)
 
+
 @blueprint_authz.route("/api/authz/user/list")
 def list_user():
     return "test"
+
 
 @blueprint_authz.route("/api/authz/user/login", methods=["POST","OPTIONS"])
 def login():
@@ -29,6 +31,29 @@ def login():
     username=args['username']
     password=args['password']
     logger.info("Verify UserLogin: %s",username)
-
     return jsonify({"code":200,"data":{"token":"asdfasdfasdfasdf12312313"}})
 
+
+@blueprint_authz.route("/api/authz/user/info",methods=["GET","OPTIONS"])
+def get_user_info():
+    args=request.args
+    token=args["token"]
+    logger.info("Get userInfo : %s",args)
+    return jsonify(
+        {"code":200,
+         "data":{
+            "roles": ['admin'],
+            "introduction": 'I am a super administrator',
+            "avatar": 'https://wpimg.wallstcn.com/f778738c-e4f8-4870-b634-56703b4acafe.gif',
+            "name": 'Super Admin'
+            }
+         }
+    )
+
+
+@blueprint_authz.route("/api/authz/user/logout",methods=["POST","OPTIONS"])
+def logout():
+    return jsonify({
+        "code": 200,
+        "data": 'success'
+    })
